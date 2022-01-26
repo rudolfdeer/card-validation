2d1bfe238d8fb6bdd3725d29da5682f2bd1314ac
Toma Lichko <tomalichko@gmail.com>
2022-01-26 15:46:25 +0300

    feat: add side changing

--- card-validation/src/components/Card/index.jsx
+++ card-validation/src/components/Card/index.jsx
@@ -1,21 +1,33 @@
 import React from 'react';
 import './index.scss';
 
-export default function Card({ values }) {
+export default function Card({ values, side }) {
   const { name, cardnumber, expireMM, expireYY, securitycode } = values;
 
   return (
     <div className="field card">
-      <div className="card__number">
-        {cardnumber ? cardnumber : '0000 0000 0000 0000'}
-      </div>
-      <div className="card__expiry-date">
-        {expireMM ? expireMM : '01'}/{expireYY ? expireYY : '01'}
-      </div>
-      <div className="card__owner">
-        {name ? name.toUpperCase() : 'CARDHOLDERS NAME'}
-      </div>
-      <img className="card__logo" src="/visa-logo.png" alt="visa logo" />
+      {side === 'front' ? (
+        <div className="card__front">
+          <div className="card__number">
+            {cardnumber ? cardnumber : '0000 0000 0000 0000'}
+          </div>
+          <div className="card__expiry-date">
+            {expireMM ? expireMM : '01'}/{expireYY ? expireYY : '01'}
+          </div>
+          <div className="card__owner">
+            {name ? name.toUpperCase() : 'CARDHOLDERS NAME'}
+          </div>
+          <img className="card__logo" src="/visa-logo.png" alt="visa logo" />
+        </div>
+      ) : (
+        <div className="card__back">
+          <div class="card__strip"></div>
+          <div className="card__container">
+            <div class="card__signature"></div>
+            <div class="card__ccv">{securitycode ? securitycode : '###'}</div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
